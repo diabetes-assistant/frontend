024ff2c037cc19c2a1d266b74ae0122cedebbe3f
Daniel Schruhl <daniel.schruhl@gropyus.com>
2021-08-01 18:49:36 +0200

    Fix linter

--- src/patient/presentation/Dashboard.tsx
+++ src/patient/presentation/Dashboard.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import styles from './Dashboard.module.css';
 
 export function Dashboard(_props: any): JSX.Element {
-    return (
-        <section className={styles.patientManagement}>
-            <h1>Patient Management</h1>
-        </section>
-    );
+  return (
+    <section className={styles.patientManagement}>
+      <h1>Patient Management</h1>
+    </section>
+  );
 }
